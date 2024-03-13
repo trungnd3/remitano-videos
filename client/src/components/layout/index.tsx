011fa9fec451967e8fb712526eb9f025338ee9c0
Trung Nguyen <trung_n@hcl.com>
2024-03-13 15:18:39 +0700

    implement share page

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './footer';
 import Header from './header';
+import { Toaster } from '@/components/ui/toaster';
 
 export default function Layout({
   children,
@@ -8,12 +9,15 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className='relative flex min-h-screen flex-col bg-background'>
-      <Header />
-      <main className='flex-1'>
-        <div className='container mt-4'>{children}</div>
-      </main>
-      <Footer />
-    </div>
+    <>
+      <div className='relative flex min-h-screen flex-col bg-background'>
+        <Header />
+        <main className='flex-1'>
+          <div className='container mt-4'>{children}</div>
+        </main>
+        <Footer />
+      </div>
+      <Toaster />
+    </>
   );
 }
--- client/src/pages/Share.tsx
+++ client/src/pages/Share.tsx
@@ -1,3 +1,57 @@
+import { FormEventHandler, useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Heading } from '@/components/common';
+import { useToast } from '@/components/ui/use-toast';
+
 export default function Share() {
-  return <div>Share</div>;
+  const [url, setUrl] = useState('');
+  const { toast } = useToast();
+
+  const submitHandler: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    if (!url.trim()) {
+      toast({
+        title: 'YouTube URL must not be empty.',
+      });
+
+      return;
+    }
+
+    if (!url.match(/(?:https:\/\/www\.)?youtube\.com\/watch\?v=\S+/gm)) {
+      toast({
+        title: 'YouTube URL not valid.',
+      });
+
+      return;
+    }
+
+    console.log(url);
+  };
+
+  return (
+    <>
+      <Heading
+        title='Share your video'
+        description='Input the YouTube URL here to share with your friends.'
+      />
+      <div className='pt-8'>
+        <form
+          onSubmit={submitHandler}
+          className='flex flex-col md:flex-row justify-center gap-4'
+        >
+          <Input
+            type='text'
+            id='ytUrl'
+            placeholder='YouTube URL'
+            value={url}
+            onChange={(event) => setUrl(event.target.value)}
+          />
+          <Button type='submit'>Share</Button>
+        </form>
+      </div>
+    </>
+  );
 }
